1035f26c55adc0baf3427f27159c12ad353b71e7
gk258007 <gk258007@gmail.com>
2024-09-10 00:11:52 +0530

    Done

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -30,8 +30,8 @@ export default function Home({
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">  
         {
-          repo.results.map((item:any) => (
-            <div style={{ display: "grid", gridTemplateColumns:200}}>
+          repo.results.map((item) => (
+            <div style={{ display: "grid", gridTemplateColumns:200}} key=''>
               {item.original_title}
               <img
                 key=''
